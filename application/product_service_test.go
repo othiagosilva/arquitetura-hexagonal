6034c6c63d76792a9e07bdcb0b89c43f7501f5f5
Thiago <othiagosilva.dev@gmail.com>
2024-01-31 15:47:09 -0300

    product service finalization

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -1,9 +1,11 @@
 package application_test
 
 import (
-	"testing"
-	"github.com/stretchr/testify/require"
+	"github.com/othiagosilva/arquitetura-hexagonal/application"
+	mock_application "github.com/othiagosilva/arquitetura-hexagonal/application/mocks"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 func TestProductService_Get(t *testing.T) {
@@ -11,7 +13,7 @@ func TestProductService_Get(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
-	product : mock_application.NewMockProductInterface(mockCtrl)
+	product := mock_application.NewMockProductInterface(mockCtrl)
 	persistence := mock_application.NewMockProductPersistenceInterface(mockCtrl)
 	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
 
@@ -20,4 +22,39 @@ func TestProductService_Get(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, product, result)
 
+}
+
+func TestProductService_Create(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	product := mock_application.NewMockProductInterface(mockCtrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(mockCtrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+	service := application.ProductService{Persistence: persistence}
+
+	result, err := service.Create("Product 1", 10)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
+
+func TestProductService_EnableDisable(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	product := mock_application.NewMockProductInterface(mockCtrl)
+	product.EXPECT().Enable().Return(nil)
+	product.EXPECT().Disable().Return(nil)
+
+	persistence := mock_application.NewMockProductPersistenceInterface(mockCtrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+	service := application.ProductService{Persistence: persistence}
+
+	result, err := service.Enable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+	result, err = service.Disable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
 }
